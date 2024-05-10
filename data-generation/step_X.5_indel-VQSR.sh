9cbe043c0eff4c54f50359634a9889937bd05394
Gwenna Breton <Gwenna.breton@gu.se>
2024-05-10 18:09:01 +0200

    Update step_X.5_indel-VQSR.sh

--- data-generation/step_X.5_indel-VQSR.sh
+++ data-generation/step_X.5_indel-VQSR.sh
@@ -7,38 +7,43 @@
 ## ApplyRecalibration: output of step_X.4_SNP-VQSR.sh
 #Output: SNP & indels VQSRed multi-sample VCF
 
-tranche=99.9
+#Program version: GATK/3.7
+reference_hg38=/path/to/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa.alt
+
 inroot=25KS.49RHG.105comp.HCBPresolution.GenotypeGVCFsallsites
-mills=/b38/hg38bundle/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
-dbsnp=/dbsnp151/human_9606_b151_GRCh38p7/00-All.newchrnames.db151.vcf.gz
+tranche=99.9
 out=25KS.49RHG.105comp.HCBPresolution.GenotypeGVCFsallsites.1-22X.recalSNP${tranche}.recalINDEL
+
+# Step 1: Variant Recalibrator
+mills=/hg38bundle/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+dbsnp=/dbsnp151/human_9606_b151_GRCh38p7/00-All.newchrnames.db151.vcf.gz
 
 java -Xmx24g -jar $GATK_HOME/GenomeAnalysisTK.jar \
 	-T VariantRecalibrator \
-	-R $ref \
--input ${inroot}.1.recalSNP${tranche}.vcf.gz \
--input ${inroot}.2.recalSNP${tranche}.vcf.gz \
--input ${inroot}.3.recalSNP${tranche}.vcf.gz \
--input ${inroot}.4.recalSNP${tranche}.vcf.gz \
--input ${inroot}.5.recalSNP${tranche}.vcf.gz \
--input ${inroot}.6.recalSNP${tranche}.vcf.gz \
--input ${inroot}.7.recalSNP${tranche}.vcf.gz \
--input ${inroot}.8.recalSNP${tranche}.vcf.gz \
--input ${inroot}.9.recalSNP${tranche}.vcf.gz \
--input ${inroot}.10.recalSNP${tranche}.vcf.gz \
--input ${inroot}.11.recalSNP${tranche}.vcf.gz \
--input ${inroot}.12.recalSNP${tranche}.vcf.gz \
--input ${inroot}.13.recalSNP${tranche}.vcf.gz \
--input ${inroot}.14.recalSNP${tranche}.vcf.gz \
--input ${inroot}.15.recalSNP${tranche}.vcf.gz \
--input ${inroot}.16.recalSNP${tranche}.vcf.gz \
--input ${inroot}.17.recalSNP${tranche}.vcf.gz \
--input ${inroot}.18.recalSNP${tranche}.vcf.gz \
--input ${inroot}.19.recalSNP${tranche}.vcf.gz \
--input ${inroot}.20.recalSNP${tranche}.vcf.gz \
--input ${inroot}.21.recalSNP${tranche}.vcf.gz \
--input ${inroot}.22.recalSNP${tranche}.vcf.gz \
-	-input /proj/snic2020-2-10/uppstore2017183/b2012165_nobackup/private/Seq_project_cont/Gwenna_Xchr/VariantCalling/ALLDATA_X_JG.20190808.179ind.recalSNP${tranche}.vcf.gz \
+	-R ${reference_hg38} \
+	-input ${inroot}.1.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.2.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.3.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.4.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.5.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.6.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.7.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.8.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.9.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.10.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.11.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.12.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.13.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.14.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.15.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.16.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.17.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.18.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.19.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.20.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.21.recalSNP${tranche}.vcf.gz \
+	-input ${inroot}.22.recalSNP${tranche}.vcf.gz \
+	-input ALLDATA_X_JG.179ind.recalSNP${tranche}.vcf.gz \
 	-resource:mills,known=false,training=true,truth=true,prior=12.0 $mills \
 	-resource:dbsnp,known=true,training=false,truth=false,prior=2.0 $dbsnp \
 	-an DP \
@@ -53,11 +58,20 @@ java -Xmx24g -jar $GATK_HOME/GenomeAnalysisTK.jar \
 	-tranchesFile ${out}.tranches \
 	-nt 4
 
-#Submit ApplyRecalibration
-cd /proj/snic2020-2-10/uppstore2017183/b2012165_nobackup/private/Seq_project_cont/Gwenna_Xchr/VQSR
-sbatch ApplyRecalibration_INDEL_25KS.49RHG.105comp_X.sh
+# Step 2: Apply recalibration
+trancheindel=99.0
+input=ALLDATA_X_JG.179ind.recalSNP${tranche}.vcf.gz
+out=25KS.49RHG.105comp.HCBPresolution.GenotypeGVCFsallsites.1-22X.recalSNP${tranche}.recalINDEL
+output=ALLDATA_X_JG.179ind.recalSNP${tranche}.recalINDEL${trancheindel}.vcf.gz
 
-
-
-
-
+java -Xmx6g -jar $GATK_HOME/GenomeAnalysisTK.jar \
+	-T ApplyRecalibration \
+	-R ${reference_hg38} \
+	-input $input \
+	-mode INDEL \
+	--ts_filter_level ${trancheindel} \
+	-recalFile $out.recal \
+	-tranchesFile $out.tranches \
+	-o $output \
+	-nt 1 \
+	-L chrX
